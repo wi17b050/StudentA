f6447023eb3836f88b5151327aad05bba58241b4
Jules <julija_palatin@gmx.at>
2017-10-05 18:45:00 +0200

    Hallo! Änderung 1

--- StudentA/StudentA/Program.cs
+++ StudentA/StudentA/Program.cs
@@ -10,7 +10,7 @@ namespace StudentA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Armin!");
         }
     }
 }
